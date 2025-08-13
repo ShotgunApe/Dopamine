cc757e33701c2b66c38d41ce04eecf33b60b15f3
ShotgunApe <shotgunayyype@gmail.com>
2025-08-13 18:54:53 -0400

    test framework compiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
+#define DOCTEST_CONFIG_COLORS_NONE
+#define DOCTEST_CONFIG_NO_MULTI_LANE_ATOMICS
+#define DOCTEST_CONFIG_NO_POSIX_SIGNALS
+#define DOCTEST_CONFIG_IMPLEMENT
+
 #include <psp2/kernel/processmgr.h>
 #include "ui.h"
 #include "emu.h"
 
+#include "doctest.h"
+#include "../test/test_r5900.h"
+
 int main(int argc, char *argv[]) {
+
+	// run tests
+	doctest::Context context;
+	context.applyCommandLine(argc, argv);
+	int res = context.run();
+
 	// init ui
 	Frontend ui;
 	ui.initFrontend();
---
+++ test/test_r5900.h
@@ -0,0 +1,10 @@
+#ifndef TEST_R5900_H
+#define TEST_R5900_H
+
+#include "emu.h"
+
+TEST_CASE("Testing R5900") {
+    CHECK(1 == 1);
+}
+
+#endif
